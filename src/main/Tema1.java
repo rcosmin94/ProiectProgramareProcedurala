715dda1fd954586db1b89f518a17ae7d005bf049
rcosmin94 <r.cosmin94@gmail.com>
2020-02-03 00:02:31 +0200

    Tema 2

---
+++ src/main/Tema1.java
@@ -0,0 +1,16 @@
+package main;
+
+// Tema 1 : Va afisa a=2, b=5, c=20. Va calcula a=2 deoarece aceasta este valoare care ii este atribuita.
+
+
+public class Tema1 {
+	
+	public static void main(String [] args) {
+		int a=3;
+		int b =(a=2)*a;
+		int c=b*(b=5);
+		System.out.println("a=" + a+ ",b=" + b + ", c=" +c);
+	}
+	
+	
+}
